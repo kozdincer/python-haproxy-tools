7b75b0429daa7aa9c57a679e578feb79239b5792
Kaan Özdinçer <kaanozdincer@gmail.com>
2013-08-12 15:49:06 +0300

    * Add pyc to gitignore. * Init classes.

--- haproxy.py
+++ haproxy.py
@@ -1,18 +1,23 @@
-class HAproxyConfig():
-    def __init__(config_path):
+class HAProxyConfig():
+    def __init__(self, config_path):
         self.config_path = config_path
         self.config = self.getConfig(self.config_path)
-        self.global = self.getGlobal()
-        
+        self.globalh = self.getGlobal()
         print config_path
+        print "-----"
+        print self.config
+
+    def getSection(self, section_name)
+        return 1
 
     def getGlobal(self):
         return 1
 
     def getConfig(self, config_path):
         config = open(config_path).read()
-        return config()
+        return config
     
 
 class Global():
     def __init__(self):
+        pass
