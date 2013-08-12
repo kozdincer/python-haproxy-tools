428930466a47ef52547e516f1f31c375e13980e7
aybuke <aybuke.147@gmail.com>
2013-08-13 02:12:52 +0300

    correcting Written global partition function

--- haproxy.py
+++ haproxy.py
@@ -3,36 +3,28 @@ class HAProxyConfig():
         self.config_path = config_path
         self.config = self.getConfig(self.config_path)
         self.globalh = self.getGlobal()
-<<<<<<< HEAD
-        
-=======
->>>>>>> 7b75b0429daa7aa9c57a679e578feb79239b5792
-        print config_path
-        print "-----"
-        print self.config
 
-    def getSection(self, section_name)
-        return 1
+        print config_path
 
     def getGlobal(self):
         return 1
-    
+
     def getSection(self):
-	f= open(self.config_path)
-	lines= f.readlines()
-	
-	for line in lines:
-	    if  line.strip() == 'global':
+        f= open(self.config_path)
+        lines= f.readlines()
+
+        for line in lines:
+            if  line.strip() == 'global':
               print line
 
-	    elif line.startswith(" "):
-	       print line  
-  
-	    elif line.strip() == 'defaults':
-	       break
-        
-         
-                 
+            elif line.startswith(" "):
+               print line
+
+            elif line.strip() == 'defaults':
+               break
+
+
+
 
         return 1
 
@@ -40,12 +32,9 @@ class HAProxyConfig():
     def getConfig(self, config_path):
         config = open(config_path).read()
         return config
-    
+
 
 class Global():
     def __init__(self):
-<<<<<<< HEAD
-	return 1
-=======
-        pass
->>>>>>> 7b75b0429daa7aa9c57a679e578feb79239b5792
+        return 1
+~                  
