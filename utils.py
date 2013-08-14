367278dc40e957883f30bffd9e98008219e98d1c
aybuke <aybuke.147@gmail.com>
2013-08-14 16:36:15 +0300

    add Global_Params

--- utils.py
+++ utils.py
@@ -1,3 +1,2 @@
 SECTIONS = ['global','defaults', 'listen', 'frontend', 'backend']
-GLOBAL_PARAMS = []
-
+GLOBAL_PARAMS = ['ca-base', 'chroot', 'crt-base', 'daemon', 'gid', 'group', 'log', 'log-send-hostname', 'nbproc', 'pidfile', 'uid', 'ulimit-n', 'user', 'stats', 'node', 'description', 'unix-bind', 'maxconn', 'maxconnrate', 'maxcomprate', 'maxcompcpuusage', 'maxpipes', 'maxsslconn', 'noepoll', 'nokqueue', 'nopol', 'nosplice', 'spread-checks', 'tune.bufsize', 'tune.chksize', 'tune.comp.maxlevel', 'tune.http.cookielen', 'tune.http.maxhdr', 'tune.maxaccept', 'tune.maxpollevents', 'tune.maxrewrite', 'tune.pipesize', 'tune.rcvbuf.client', 'tune.rcvbuf.server', 'tune.sndbuf.client', 'tune.sndbuf.server', 'tune.ssl.cachesize', 'tune.ssl.lifetime', 'tune.ssl.maxrecord', 'tune.zlib.memlevel', 'tune.zlib.windowsize', 'debug', 'quiet']
