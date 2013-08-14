6e644a79d3f0718f803cb1568aa1083312cc5852
aybuke <aybuke.147@gmail.com>
2013-08-14 11:52:22 +0300

    getSelection function

--- haproxy.py
+++ haproxy.py
@@ -13,6 +13,18 @@ class HAProxyConfig():
 	f.close()
 	for line in lines:
 	    line = line.strip()
+	    
+	    if section in SECTIONS:
+		if section in line:
+                    print line
+	        elif line.startswith(" "):
+		    print line
+		elif line.startswith("#"):
+		    continue
+	        elif line in section:
+		    break 
+		else:
+		    print line 
 	return 1
 
     def getGlobal(self):
