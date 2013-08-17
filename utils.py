29407d2a593d7fe5585ffc99d641de926a36b403
aybuke <aybuke.147@gmail.com>
2013-08-17 13:44:11 +0300

    The license file has been changed to GLPv3

--- utils.py
+++ utils.py
@@ -1,3 +1,23 @@
+"""
+Copyright (C) 2013 - Aybuke Ozdemir <aybuke.147@gmail.com>
+
+This file is part of python-haproxy-tools.
+
+python-haproxy-tools is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+python-haproxy-tools is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+
+"""
+
 SECTIONS = ['global','defaults', 'listen', 'frontend', 'backend']
 GLOBAL_PARAMS = ['ca-base', 'chroot','crt-base', 'daemon', 'gid', 
 'group', 'log', 'log-send-hostname', 'nbproc', 'pidfile', 'uid', 
