bfc69e5e44bf38640c6bb12dbfc70d7f97b40b78
aybuke <aybuke.147@gmail.com>
2013-08-29 21:11:03 +0300

    Add getx functions for proxies.

--- haproxy.py
+++ haproxy.py
@@ -23,27 +23,66 @@ class HAProxyConfig():
 
     def __init__(self, config_path):
         self.config_path = config_path
-        self.config = self.readConfig()
+        self.config = self.__readConfig()
         self.globalh = Global(self.__getSection('global'))
-        self.defaults = Defaults(self.__getSection('global'))
+        self.defaults = Defaults(self.__getSection('defaults'))
 
         #Set Listen.
         self.listens = []
-        for name in self.getSectionNames('listen'):
-            l = Listen(self.__getSection('listen', name))
+        for name in self.__getSectionNames('listen'):
+            l = Listen(self.__getSectionWithName('listen', name))
             self.listens.append(l)
 
         # Set Frontends.
         self.frontends = []
-        for name in self.getSectionNames('frontend'):
-            f = Frontend(self.__getSection('frontend', name))
+        for name in self.__getSectionNames('frontend'):
+            f = Frontend(self.__getSectionWithName('frontend', name))
             self.frontends.append(f)
 
         #Set Backend.
         self.backends = []
-        for name in self.getSectionNames('backend'):
-            b = Backend(self.__getSection('backend', name))
+        for name in self.__getSectionNames('backend'):
+            b = Backend(self.__getSectionWithName('backend', name))
             self.backends.append(b)
+
+    def getGlobal(self):
+        return self.globalh
+
+    def getFrontend(self, name):
+        name = name.strip()
+        for fe in self.frontends:
+            if fe.description.name == name:
+                return fe
+
+    def getBackend(self, name):
+        name = name.strip()
+        for be in self.backends:
+            if be.description.name == name:
+                return be
+
+    def getListen(self, name):
+        name = name.strip()
+        for l in self.listens:
+            if l.description.name == name:
+                return l
+
+    def getFrontends(self):
+        return self.frontends
+
+    def getBackends(self):
+        return self.backends
+
+    def getListens(self):
+        return self.listens
+
+    def __getSectionNames(self, title):
+        l_names = []
+        for row in self.config:
+            row = row.strip()
+            if row.startswith(title):
+                name = row.split()[1]
+                l_names.append(name )
+        return l_names
 
     def __getSectionWithName(self, title, name):
         config_array = []
@@ -75,32 +114,42 @@ class HAProxyConfig():
         return config_array
 
     def __getSection(self, title):
+        config_array = []
+        title_array = ['global', 'defaults']
+        title = title.strip()
+        start_flag = False
 
+        for line in self.config:
+            line = line.strip()
 
-    def getFrontend(self, name=None):
-        ca = self.__getSectionWithName('frontend', name)
-        return ca
+            if line == '':
+                continue
 
-    def readConfig(self):
+            if title in title_array:
+                start_flag = True
+                continue
+
+            if title in SECTIONS:
+                start_flag = False
+
+            if title not in title_array:
+                return []
+
+            if start_flag:
+                config_array.append(line)
+        return config_array
+
+    def __readConfig(self):
         config_file = open(self.config_path)
         config = config_file.readlines()
         config_file.close()
         return config
 
-    def getSectionNames(self, title):
-        l_names = []
-        for row in self.config:
-            row = row.strip()
-            if row.startswith(title):
-                name = row.split()[1]
-                l_names.append(name )
-        return l_names
-
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
-        return self.readConfig()
+        return self.__readConfig()
 
 class Option():
     def __init__(self, param_name, params):
@@ -120,7 +169,7 @@ class Option():
         return self.__repr__()
 
     def __repr__(self):
-        return str(self.name + " " + " ".join(self.params))
+        return self.name + " " + "".join(self.params)
 
 
 class Section():
@@ -145,7 +194,12 @@ class Section():
                 else:
                     params = None
 
-                self.description = Description(title, name, params)
+                self.description = Description(title, name)
+
+                if params:
+                    for p in params:
+                        o = Option('bind', p)
+                        self.options.append(o)
 
                 continue
 
@@ -191,10 +245,9 @@ class Section():
         return self.getConfig()
 
 class Description():
-    def __init__(self, title, name=None, params=None):
+    def __init__(self, title, name=None):
         self.title = title
         self.name = name
-        self.params = params
 
     def __str__(self):
         return self.__repr__()
@@ -204,32 +257,29 @@ class Description():
         out.append(self.title)
         if self.name:
             out.append(self.name)
-        if self.params:
-            out + self.params
         return " ".join(out)
 
 class Global(Section):
     def __init__(self, config_array):
-        print '### global'
         Section.__init__(self, config_array)
 
 class Defaults(Section):
     def __init__(self, config_array):
-        print '### defaults'
         Section.__init__(self, config_array)
 
 class Listen(Section):
     def __init__(self, config_array):
-        print '### listen'
         Section.__init__(self, config_array)
+        self.name = self.description.name
 
 class Frontend(Section):
     def __init__(self, config_array):
-        print '### frontend'
         Section.__init__(self, config_array)
+        self.name = self.description.name
 
 class Backend(Section):
     def __init__(self, config_array):
-        print '### backend'
         Section.__init__(self, config_array)
+        self.name = self.description.name
+
 
